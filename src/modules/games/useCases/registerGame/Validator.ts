33511c3130fa306a6a68e17aca5f8a1155ea080a
Tiago Damasceno <tiagodepd@yahoo.com.br>
2022-08-16 15:50:57 -0300

    feat: customize errors, and data validation with Celebrate.

--- src/modules/games/useCases/registerGame/Validator.ts
+++ src/modules/games/useCases/registerGame/Validator.ts
@@ -1,14 +1,16 @@
 import { Joi, Segments } from "celebrate";
 
+import { customMessage } from "../../../../utils/errors";
+
 const registerValidator = {
   [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    designer: Joi.string().required(),
-    genre: Joi.string().required(),
-    platform: Joi.string().required(),
-    developer: Joi.string().required(),
-    releaseData: Joi.string().required(),
-    mode: Joi.string().required()
+    name: Joi.string().required().messages(customMessage("name")),
+    designer: Joi.string().required().messages(customMessage("designer")),
+    genre: Joi.string().required().messages(customMessage("genre")),
+    platform: Joi.string().required().messages(customMessage("platform")),
+    developer: Joi.string().required().messages(customMessage("developer")),
+    releaseData: Joi.string().required().messages(customMessage("releaseData")),
+    mode: Joi.string().required().messages(customMessage("mode"))
   })
 };
 
